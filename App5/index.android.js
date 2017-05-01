4f88d01629330cda7f07b79340395b0dd5ea6f41
Thir <nick@brasilnet2000.com.br>
2017-05-01 19:59:56 -0300

    App6 configuração de ambiente
    App6 configuração de ambiente

--- App5/index.android.js
+++ App5/index.android.js
@@ -1,53 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Navigator
 } from 'react-native';
+
+import CenaPrincipal from './src/components/CenaPrincipal';
+import CenaClientes from './src/components/CenaClientes';
+import CenaContatos from './src/components/CenaContatos';
+import CenaEmpresa from './src/components/CenaEmpresa';
+import CenaServicos from './src/components/CenaServicos';
 
 export default class App5 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ id: 'principal' }}
+        renderScene={(route, navigator) => {
+          switch (route.id) {
+            case 'principal':
+              return (<CenaPrincipal navigator={navigator} />);
+
+            case 'cliente':
+              return (<CenaClientes navigator={navigator} />);
+
+            case 'contato':
+              return (<CenaContatos navigator={navigator} />);
+
+            case 'empresa': 
+              return (<CenaEmpresa navigator={navigator} />);
+
+            case 'servico':
+              return (<CenaServicos navigator={navigator} />);
+
+            default:
+              return false;
+          }
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('App5', () => App5);

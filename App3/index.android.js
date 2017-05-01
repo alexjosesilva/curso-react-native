fd9f0796431be91ecb19bfd1e033873b024a0a78
Thir <nick@brasilnet2000.com.br>
2017-05-01 03:29:20 -0300

    App3: Jogo de Pedra Papel Tesoura
    App3: Jogo de Pedra Papel Tesoura

--- App3/index.android.js
+++ App3/index.android.js
@@ -9,23 +9,100 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Button,
   View
 } from 'react-native';
 
-export default class App3 extends Component {
-  render() {
-    return (
-     			<Text></Text>
-     			<Text></Text>
-     			<Text></Text>
+class App3 extends Component{
 
-     			<Button title="pedra"> /Button>
-     			<Button title="tesoura"></Button>
-     			<Button title="papel"></Button>
+  constructor(props){
+    super(props);
+
+    this.state = {  escolhaUsuario : '', 
+                    escolhaComputador : '', 
+                    resultado : ''
+                  }
+  }
+
+  jokenpo(escolhaUsuario){
+
+    //gera número aleatório ( 0, 1, 2)
+    var numAleatorio = Math.floor(Math.random() * 3);
+
+    var escolhaComputador = '';
+
+    switch(numAleatorio){
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+    }
+
+    var resultado = '';
+
+    if(escolhaComputador == 'pedra'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Usuário ganhou';
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if(escolhaComputador == 'papel'){
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Usuário ganhou';
+      }
+
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    if(escolhaComputador == 'tesoura'){
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Usuário ganhou';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Computador ganhou';
+      }
+    }
+
+    this.setState({ escolhaUsuario : escolhaUsuario,
+                    escolhaComputador : escolhaComputador,
+                    resultado : resultado
+                  });
+
+
+  }
+
+  render(){
+    return (
+      <View>
+        <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
+        <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
+        <Text>Resultado: {this.state.resultado}</Text>
+        <Button title="pedra" onPress={ () => { this.jokenpo('pedra')}} />
+        <Button title="papel" onPress={ () => { this.jokenpo('papel')}} />
+        <Button title="tesoura" onPress={ () => { this.jokenpo('tesoura')}} />
+      </View>
     );
   }
 }
 
-
+ 
 
 AppRegistry.registerComponent('App3', () => App3);
